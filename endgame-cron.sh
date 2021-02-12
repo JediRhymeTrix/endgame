aa23bdca64928d210d3fb163ad4943a930110a37
Vedant Vohra <ved914980@gmail.com>
2021-02-12 15:50:31 +0530

    Add files via upload

---
+++ endgame-cron.sh
@@ -0,0 +1,1 @@
+echo $(date) "|" $(/bin/bash /bin/endgame.sh https://in.bookmyshow.com/hyderabad/movies/avengers-endgame/ET00090482 "showtimes btn _cuatro" endgame_tickets_available hJg0yIw1CDDu65sbA0Oc9oIM4Sc4C2GJFrUKQ99b0jE nvk.681@gmail.com) >> /bin/endgame.log
